4027a69b039eacff2e594f81865b0f8cc0dd0956
LiuBingbin <LiuBingbin_1@qq.com>
2021-03-03 17:16:04 +0800

    就差登录验证

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -27,8 +27,7 @@ export default {
   methods: {
     async login() {
       const res = await this.$http.post("/login", this.model);
-      console.log(res.data);
-      localStorage.token = res.data;
+      localStorage.token = res.data.token;
       this.$router.push("/");
       this.$message({
         type: "success",
